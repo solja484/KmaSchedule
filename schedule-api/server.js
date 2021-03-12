4c70735dd16e035c9ec38ac8f903bd2c3fc8caca
solja <solja484@gmail.com>
2021-03-12 12:11:12 +0200

    get schedule info from db

--- schedule-api/server.js
+++ schedule-api/server.js
@@ -1,66 +1,136 @@
-let express = require('express');
-let server = express();
+const express = require('express');
+const server = express();
 const path = require('path');
-let config = require('./config.json');
-let bodyParser = require('body-parser');
-let currentSeason = 2;
-
-
+const config = require('./config.json');
+const bodyParser = require('body-parser');
 const mysql = require("mysql2");
+
 server.use(express.static(path.join(__dirname, '../schedule-vue/build')));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
-
 
 server.listen(3080, () => {
     console.log('listening on 3080')
 });
 
-//connect to mysql database
 const connection = mysql.createConnection({
     host: config.host,
-    user: "root",
-    database: "kma_data",
-    password: "mysql"
+    user: config.user,
+    database: config.database,
+    password: config.password
 }).promise();
+
 
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
 });
 
 server.get('/api/faculty', (req, res) => {
-    connection.query("SELECT * FROM faculty").then(
-        ([results, fields]) =>
-            res.json(results));
+    connection.query("SELECT * FROM faculty")
+        .then(([results, fields]) =>
+            res.json(results))
+        .catch(err =>
+            console.log(err));
 });
-server.get('/api/subfaculty', (req, res) => {
-    connection.query("SELECT * FROM subfaculty ORDER BY faculty_id").then(
-        ([results, fields]) =>
-            res.json(results));
+server.get('/api/sub_faculty', (req, res) => {
+    connection.query("SELECT * FROM subfaculty ORDER BY faculty_id")
+        .then(([results, fields]) =>
+            res.json(results))
+        .catch(err =>
+            console.log(err));
 });
 server.get('/api/speciality', (req, res) => {
-    connection.query("SELECT DISTINCT name, id, faculty_id, level FROM speciality " +
-        "ORDER BY speciality.level").then(
-        ([results, fields]) =>
-            res.json(results));
+    connection.query("SELECT DISTINCT name, id, faculty_id, level " +
+        "FROM speciality ORDER BY speciality.level")
+        .then(([results, fields]) =>
+            res.json(results))
+        .catch(err =>
+            console.log(err));
 });
 
-server.get('/api/user', (req, res) => {
-    connection.query("SELECT * FROM _user WHERE code='204186'").then(
-        ([results, fields]) =>
-            res.json(results));
+server.get('/api/methodist/schedules', (req, res) => {
+    connection.query("SELECT * FROM schedule WHERE faculty_id=? AND academic_year=? AND season=?",
+        [req.query.faculty_id, req.query.year])
+        .then(([results, fields]) =>
+            res.json(results))
+        .catch(err =>
+            console.log(err));
+});
+
+server.get('/api/schedules', (req, res) => {
+    connection.query("SELECT * FROM schedule " +
+        "WHERE draft=? AND academic_year=? AND season=?", [0, req.query.year, req.query.season])
+        .then(([results, fields]) =>
+            res.json(results))
+        .catch(err =>
+            console.log(err));
+});
+
+server.get('/api/schedule/:code', (req, res) => {
+    connection.query("SELECT schedule.id,schedule.code,schedule.faculty_id, faculty.name as faculty, schedule_type, " +
+        "academic_year, season, schedule.level, study_year, title, subfaculty.name as subfaculty, " +
+        "speciality.name as speciality FROM schedule " +
+        "LEFT JOIN faculty ON schedule.faculty_id=faculty.id " +
+        "LEFT JOIN kma_data.subfaculty ON kma_data.subfaculty.id=kma_data.schedule.subfaculty_id " +
+        "LEFT JOIN kma_data.speciality ON kma_data.speciality.id=kma_data.schedule.speciality_id " +
+        "WHERE code=? ", [req.params.code])
+        .then(([results, fields]) =>
+            connection.query("SELECT course_schedule.id, course_schedule.course_cdoc as course_code, " +
+                "course_schedule.group, day_id, pair_id, lection, weeks, classroom.number as classroom, " +
+                "course.name as name, course.teacher as teacher FROM course_schedule " +
+                "LEFT JOIN course_season ON course_schedule.course_cdoc=course_season.course_cdoc " +
+                "LEFT JOIN course ON course.sub_cdoc=course_schedule.course_cdoc " +
+                "LEFT JOIN classroom ON course_schedule.classroom=classroom.id " +
+                "WHERE schedule_code=? ORDER BY day_id", [req.params.code]).then(([results2, fields2]) =>
+                res.json({"schedule": results[0], "courses": results2}))
+                .catch(err =>
+                    console.log(err)))
+        .catch(err =>
+            console.log(err));
+});
+
+server.post('/api/login', (req, res) => {
+    connection.query("SELECT id, link, role, code, name " +
+        "FROM _user WHERE login=? AND password=?", [req.body.login, req.body.password])
+        .then(([results, fields]) => {
+            if (results[0].role == 'metodist_dec')
+                connection.query("SELECT faculty_id, faculty_name " +
+                    "FROM _metodist WHERE id=?", [results[0].link])
+                    .then(([results2, fields2]) => {
+                        results[0].methodist = results2[0];
+                        console.log(results[0]);
+                        res.json(results[0]);
+                    }).catch(err =>
+                    console.log(err));
+            else if (results[0].role == 'student')
+                connection.query("SELECT spec_id, speciality.name as speciality, " +
+                    "faculty_id, faculty.name as faculty_name FROM student " +
+                    "INNER JOIN speciality ON student.spec_id=speciality.id " +
+                    "INNER JOIN faculty ON speciality.faculty_id=faculty.id " +
+                    "WHERE student.id=?", [results[0].link])
+                    .then(([results2, fields2]) => {
+                        results[0].student = results2[0];
+                        console.log(results[0]);
+                        res.json(results);
+                    }).catch(err =>
+                    console.log(err));
+        }).catch(err =>
+        console.log(err));
 });
 
 server.get('/api/days', (req, res) => {
-    connection.query("SELECT * FROM working_days ORDER BY number").then(
-        ([results, fields]) =>
-            res.json(results));
+    connection.query("SELECT * FROM working_days ORDER BY number")
+        .then(([results, _]) =>
+            res.json(results))
+        .catch(err =>
+            console.log(err));
 });
 
 server.get('/api/pairs', (req, res) => {
-    connection.query("SELECT * FROM pairs ORDER BY number").then(
-        ([results, fields]) =>
-            res.json(results));
+    connection.query("SELECT * FROM pairs ORDER BY number")
+        .then(([results, _]) =>
+            res.json(results)).catch(err =>
+        console.log(err));
 });
 
 server.get('/api/user/:usercode/courses', (req, res) => {
@@ -70,16 +140,21 @@ server.get('/api/user/:usercode/courses', (req, res) => {
         "INNER JOIN course ON course_season.course_cdoc=course.sub_cdoc " +
         "INNER JOIN course_register ON course_season.course_cdoc=course_register.sub_code " +
         "WHERE course_season.season='2' AND academic_year='2020' AND " +
-        "course_register.user_code='204186'AND course_register.deleted='0';").then(
-        ([results, fields]) =>
-            connection.query("SELECT course_schedule.id as id, course_cdoc, course_schedule.group, lection, pair_id, day_id, weeks, classroom.number FROM course_schedule " +
+        "course_register.user_code='204186'AND course_register.deleted='0';")
+        .then(([results, fields]) =>
+            connection.query("SELECT course_schedule.id as id, course_cdoc, course_schedule.group, " +
+                "lection, pair_id, day_id, weeks, classroom.number FROM course_schedule " +
                 "LEFT JOIN classroom ON course_schedule.classroom = classroom.id " +
                 "WHERE course_cdoc IN " +
                 "(SELECT course_cdoc FROM course_season " +
                 "INNER JOIN course ON course_season.course_cdoc=course.sub_cdoc " +
-                "WHERE course_season.season='2' AND academic_year='2020' AND status_happened='happened' AND course_cdoc IN " +
-                "(SELECT sub_code FROM kma_data.course_register WHERE user_code='204186'AND deleted='0'));").then(
-                    ([results2, fields2]) =>res.json({"course_data":results,"course_schedule":results2})));
-
-
+                "WHERE course_season.season='2' AND academic_year='2020' AND status_happened='happened' " +
+                "AND course_cdoc IN " +
+                "(SELECT sub_code FROM kma_data.course_register WHERE user_code='204186'AND deleted='0'));")
+                .then(([results2, fields2]) =>
+                    res.json({"course_data": results, "course_schedule": results2}))
+                .catch(err =>
+                    console.log(err)))
+        .catch(err =>
+            console.log(err));
 });
